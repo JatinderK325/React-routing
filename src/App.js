2757ccd96571fc6b793db9d8fc8407aaf212c928
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2022-08-10 00:25:55 -0400

    nested route in welcome component added

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ function App() {
     <div>
       <header><MainHeader /></header>
       <main>
+        {/* Switch component is used to make one route active at a time becoz by default all routes that match the current path will be loaded. for eg: if path is '/products/:productId', both 'products' as well as 'product detail 'will be loaded. 
+        React-router matches the path from the start hence it gives both the components on the screen.
+        hence, to make only one route active, Switch is used. with this, react-router will go through from top to bottom. when it finds the first match, it will display the route and stop searching.*/}
         <Switch>
           <Route path="/welcome">
             <Welcome />
           </Route>
-          <Route path="/products" >
+          {/* exact tells react-router that 'Products' shd only lead to match if we have an exact match so it then switches from matching the beginning of the path to matching the full path.
+          Means, if we are searching path="/products/:productId" then just becoz the beginning of both the pages is same, it should not render the 'Products' until the path="/products".   */}
+          <Route path="/products" exact >
             <Products />
           </Route>
           <Route path="/products/:productId">
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,6 +1,12 @@
+import { Route } from "react-router-dom";
+
 const Welcome = () => {
     return (
-        <h1>The Welcome Page</h1>
+        <section>
+            <h1>The Welcome Page</h1>
+            {/* this route is nested route becoz its inside a 'Welcome' page. Only if welcome page is active, this new-user route will be evaluated by react-router. therefore when we defined the path for new route 'new-user', we started with the '/welcome' in order to activate the welcome page and hence 'new-user' route can also be active with '/new-user'. */}
+            <Route path='/welcome/new-user'><p>Welcome, new user!</p></Route>
+        </section>
     );
 };
 
